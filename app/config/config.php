d0fb9febba84e3cb89923f5a2fee526d891643f2
JociroPy <clarosrocajosue@gmail.com>
2025-09-02 02:57:36 -0400

    Change BASE_URL and email configuration
    Updated BASE_URL and email credentials in config.php.

--- app/config/config.php
+++ app/config/config.php
@@ -3,13 +3,13 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'CambaNet');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('BASE_URL', 'http://172.20.10.3/CambaNet/public');
+define('BASE_URL', 'http://localhost/CambaNet/public');
 define('SITE_NAME', 'CambaNet');
 define('EMAIL_HOST', 'smtp.gmail.com');
 define('EMAIL_PORT', 587);
-define('EMAIL_USERNAME', 'clarosrocajosue@gmail.com');
-define('EMAIL_PASSWORD', 'yljowzoaufayfjjw');
-define('EMAIL_FROM', 'clarosrocajosue@gmail.com');
+define('EMAIL_USERNAME', 'tucorreo@gmail.com');
+define('EMAIL_PASSWORD', 'tucredencial');
+define('EMAIL_FROM', 'tucorreo@gmail.com');
 define('EMAIL_FROM_NAME', 'CambaNet');
 function url($action = '') {
     return BASE_URL . '/?action=' . $action;
